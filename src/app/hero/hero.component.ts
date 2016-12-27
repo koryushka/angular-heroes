0672cabcf9e8888526f4c08d9028b5b15c916e95
Kiryl Karetnikau <kiryl_karetnikau@epam.com>
2016-12-27 17:49:15 +0300

    Init commit

---
+++ src/app/hero/hero.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero'
+import { HEROES } from './hero'
+
+
+@Component({
+  selector: 'app-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+export class HeroComponent implements OnInit {
+  selectedHero: Hero;
+
+
+  heroes = HEROES;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  selectHero(hero: Hero): void{
+    this.selectedHero = hero;
+    console.debug("Hero: ", hero)
+  }
+
+}
